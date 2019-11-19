ef5790e82236f73ca3c09ed675e98e6d91cfb2f1
aximilli1212 <aximilli1212@gmail.com>
2019-11-19 09:05:59 +0000

    Get books method implemented

--- bookAPI/main.go
+++ bookAPI/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
@@ -10,12 +11,48 @@ import (
 
 type Book struct{
 	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Author *Author `json:"author"`
+}
+
+// Author Struct
+  type Author struct {
+  	Firstname string `json:"firstname"`
+  	Lastname string `json:"lastname"`
+  }
+
+// init books var as slice Book struct
+var books []Book
+
+// Get all Books
+func getBooks(w http.ResponseWriter, r *http.Request){
+        w.Header().Set("Content-Type", "application/json")
+        json.NewEncoder(w).Encode(books)
+}// Get all Books
+func getBook(w http.ResponseWriter, r *http.Request){
+
+}// Get all Books
+func createBook(w http.ResponseWriter, r *http.Request){
+
+}// Get all Books
+func updateBooks(w http.ResponseWriter, r *http.Request){
+
+}// Get all Books
+func deleteBooks(w http.ResponseWriter, r *http.Request){
+
 }
 
 func main(){
 	//Init mux router
 
 	r := mux.NewRouter() // : type inference
+
+
+
+	// Mock Data - implement DB
+	books = append(books, Book{ID:"1",Isbn:"338383", Title:"Sweet Victory", Author: &Author{Firstname:"John", Lastname: "Doe"}})
+	books = append(books, Book{ID:"1",Isbn:"338383", Title:"Swoet Victory", Author: &Author{Firstname:"John", Lastname: "Doe"}})
 
 	//Router handler / Endpoints
 
