39ef81974b741693311b89084a3d566910c9a7ec
aximilli1212 <aximilli1212@gmail.com>
2019-11-21 08:27:11 +0000

    Add Create book route and handler

--- bookAPI/main.go
+++ bookAPI/main.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"github.com/gorilla/mux"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 )
 
 //Book Struct(Model)
@@ -46,6 +48,12 @@ func getBook(w http.ResponseWriter, r *http.Request){
 
 }// Get all Books
 func createBook(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+  var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(1000000)) //Mock ID - not Safe
+	books = append(books,book)
+	json.NewEncoder(w).Encode(&Book{})
 
 }// Get all Books
 func updateBooks(w http.ResponseWriter, r *http.Request){
